13102c15f82ef6e8621bbeeb99f44b04a1a43650
Lilinamay <rc4333@nyu.edu>
2022-02-01 08:48:01 -0500

    inclass

--- TestProject/Assets/MayStuff/script/fire.cs
+++ TestProject/Assets/MayStuff/script/fire.cs
@@ -9,17 +9,18 @@ public class fire : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("running");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //this.transform.position = new Vector3(this.transform.position.x - .1f, this.transform.position.y, this.transform.position.z);
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("hit");
         if(other.gameObject== pBox)
         {
             trigger = true;
