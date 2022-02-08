898d694adc8145015d5b7eb7f376a1eef76b2654
Lilinamay <rc4333@nyu.edu>
2022-02-08 07:52:37 -0500

    inclass

---
+++ TestProject/Assets/MayStuff/script/PlayerTrigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Renderer render = GetComponent<Renderer>();
+        render.material.color = Color.green;
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+}
