a4c282495de357f61537bb2d25df82dfa389c6ea
Lilinamay <rc4333@nyu.edu>
2022-02-26 22:13:36 -0500

    camera

--- TestProject/Assets/MayStuff/homeScript/snowball.cs
+++ TestProject/Assets/MayStuff/homeScript/snowball.cs
@@ -5,24 +5,28 @@ using UnityEngine;
 public class snowball : MonoBehaviour
 {
     private Rigidbody snowbody;
+    Cinemachine.CinemachineImpulseSource source;
     //[SerializeField] GameObject gameManager;
     //playerShoot playershoot;
     // Start is called before the first frame update
     private void Awake()
     {
         snowbody = GetComponent<Rigidbody>();
+        source = GetComponent<Cinemachine.CinemachineImpulseSource>();
         //playershoot = ;
     }
     void Start()
     {
         float speed = 20f;
         snowbody.velocity = transform.forward * speed;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //snowbody.AddForce(transform.forward * (100 * Random.Range(1.3f, 1.7f)), ForceMode.Impulse);
+        source.GenerateImpulse(Camera.main.transform.forward);
     }
 
     private void OnTriggerEnter(Collider other)
